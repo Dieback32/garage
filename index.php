91da9dd430bf1c45bee4c2c64deeb32a11398fd6
DIEBACK\Dieback <rarecandy05@gmail.com>
2025-08-24 08:56:41 +0800

    Update

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Checkout Demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h1>Welcome to Checkout Demo</h1>
+    <p>Click below to begin the checkout process.</p>
+    <a href="user_data1.php" class="btn">Start Checkout</a>
+</div>
+</body>
+</html>
